fee19f1fbf8d229fb32835c0d16316794131a473
Lilit Sargsyan <lilit_sargsyan@epam.com>
2020-04-19 23:04:13 +0400

    added csv to json convertion

--- Homework2/src/index.js
+++ Homework2/src/index.js
@@ -1,6 +1,5 @@
 const readline = require('readline');
 const fs = require('fs');
-const lineReader = require('line-reader');
 const csvtojson = require("csvtojson");
 
 // Task 1.1
@@ -11,59 +10,21 @@ const rl = readline.createInterface({
 });
 
 const reverseString = (str) => str.split("").reverse().join("");
-
+console.log("Enter something...");
 rl.on('line', (line) => {
   console.log(reverseString(line));
+  console.log("Enter something...");
 });
 
 // Task 2
 const csvPath = 'src/data/nodejs-hw1-ex1.csv';
 const jsonPath = 'src/data/nodejs-hw1-ex2.txt';
 
-// const readInterface = readline.createInterface({
-//   input: fs.createReadStream(csvPath),
-//   output: process.stdout,
-//   console: false
-// });
-
-const csvJson = csvtojson();
-const writable = fs.createWriteStream(jsonPath);
-let lineInJson;
 try {
-lineReader.eachLine(csvPath, (line) => {
-  console.log("------------ ", line);
-    console.log("w ***** ", writable);
-      csvJson.fromString(line).pipe(writable);
-      // lineInJson = await csvJson.fromString(line);
-      // writable.write(lineInJson);
-});
+  const readable = fs.createReadStream(csvPath);
+  const writable = fs.createWriteStream(jsonPath);
+  const csvJson = csvtojson();
+  readable.pipe(csvJson).pipe(writable);
 } catch (er) {
-  console.log("ERROR: ", er);
+  throw new Error(`While reading file: ${csvPath} occurred an error: ${er}`);
 }
-
-// const writable = fs.createWriteStream(jsonPath);
-// lineReader.open(csvPath, (err, reader) => {
-//   if (reader.hasNextLine()) {
-//     reader.nextLine((err, line) => {
-//       if (err) throw new Error(`An error occurred while reading file ${csvPath}: ` + err);
-//       console.log("aaaa", line);
-//       writable.write(line);
-//     });
-//   } else {
-//     console.log("aaaaaaaaaaaaaa");
-//   }
-// });
-
-
-// const readable = fs.createReadStream(csvPath);
-// const writable = fs.createWriteStream(jsonPath);
-// const csvJson = csvtojson();
-// readable.pipe(csvJson).pipe(writable);
-//------------------------------------
-// // try {
-//   readInterface.on('line', function (line) {
-//     console.log(line);
-//   });
-// // } catch (er) {
-// //   throw new Error(`While reading file: ${csvPath} occurred an error: ${er}`);
-// // }
